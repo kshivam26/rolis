ae5b686e4eb93708fc4ef9ea976744fe677e7d41
Kumar Shivam <kumarshivam.shivam@gmail.com>
2023-09-13 16:22:10 +0000

    changed the ip addresses

--- third-party/paxos/config/1silo_1paxos_1follower/run.sh
+++ third-party/paxos/config/1silo_1paxos_1follower/run.sh
@@ -1,3 +1,3 @@
 git checkout .
-sed -i "s/localhost: 127.0.0.1/localhost: 10.1.0.7/g" `grep "localhost: 127.0.0.1" -rl ./*.yml`
-sed -i "s/p1: 127.0.0.1/p1: 10.1.0.8/g" `grep "p1: 127.0.0.1" -rl ./*.yml`
+sed -i "s/localhost: 127.0.0.1/localhost: 172.17.0.5/g" `grep "localhost: 127.0.0.1" -rl ./*.yml`
+sed -i "s/p1: 127.0.0.1/p1: 172.17.0.6/g" `grep "p1: 127.0.0.1" -rl ./*.yml`
