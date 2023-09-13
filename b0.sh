565a47a64b5ca5fdf9036111e1655257329bb45c
Kumar Shivam <kumarshivam.shivam@gmail.com>
2023-09-13 17:58:59 +0000

    test crpc

--- b0.sh
+++ b0.sh
@@ -10,6 +10,6 @@ batch=${2:-$defvalue}
 # sudo cgset -r cpuset.mems=0 cpulimitl
 # sudo cgset -r cpuset.cpus=0-$trd cpulimitl
 # sudo cgexec -g cpuset:cpulimitl ./out-perf.masstree/benchmarks/dbtest --verbose --bench tpcc --db-type mbta --scale-factor $trd --num-threads $trd --numa-memory 1G --parallel-loading --runtime 30 --bench-opts="--cpu-gap 1 --num-cpus 32" -F third-party/paxos/config/1silo_1paxos_2follower/$yyml.yml -F third-party/paxos/config/occ_paxos.yml --paxos-leader-config --multi-process -P localhost -S $batch # > ./xxxx15/leader-$trd-$batch.log 2>&1 &
-sudo ./out-perf.masstree/benchmarks/dbtest --verbose --bench tpcc --db-type mbta --scale-factor $trd --num-threads $trd --numa-memory 1G --parallel-loading --runtime 150 --bench-opts="--cpu-gap 1 --num-cpus 32" -F third-party/paxos/config/1silo_1paxos_2follower/$yyml.yml -F third-party/paxos/config/occ_paxos.yml --paxos-leader-config --multi-process -P localhost -S $batch # > ./xxxx15/leader-$trd-$batch.log 2>&1 &
+sudo ./out-perf.masstree/benchmarks/dbtest --verbose --bench tpcc --db-type mbta --scale-factor $trd --num-threads $trd --numa-memory 1G --parallel-loading --runtime 30 --bench-opts="--cpu-gap 1 --num-cpus 32" -F third-party/paxos/config/1silo_1paxos_2follower/$yyml.yml -F third-party/paxos/config/occ_paxos.yml --paxos-leader-config --multi-process -P localhost -S $batch # > ./xxxx15/leader-$trd-$batch.log 2>&1 &
 
 #tail -f ./xxxx15/leader-$trd-$batch.log
