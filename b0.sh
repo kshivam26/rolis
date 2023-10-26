1bca42c44e9d34eefea0c59393aad271941945ba
Akshat Sharma <akshatsharma2602@gmail.com>
2023-10-26 14:32:25 -0400

    Reverted sh files

--- b0.sh
+++ b0.sh
@@ -1,36 +1,14 @@
 #!/bin/bash
-
-# if [ $# -lt 2 ]; then
-#   echo "Usage: $0 <num_threads> <cfs_quota_us>"
-#   exit 1
-# fi
-
-num_threads=$1
-defvalue=1000
-batch=${2:-$defvalue}
-# cfs_quota_us=$2
-
-# if [ "$cfs_quota_us" -lt 0 ] || [ "$cfs_quota_us" -gt 100 ]; then
-  # echo "cfs_quota_us must be between 0 and 100"
-  # exit 1
-# fi
-
-# total_cores=$(nproc)
-# lower_bound=0
-# upper_bound=$(($total_cores/3))
-# upper_bound=$(($upper_bound < ($num_threads + $lower_bound) ? $upper_bound : ($num_threads + $lower_bound)))
-
-# cpuset_cpus="$lower_bound-$upper_bound"
-
 # sudo cgdelete -g cpuset:/cpulimitl
 mkdir -p xxxx15
-# sudo cgcreate -t $USER:$USER -a $USER:$USER -g cpuset:/cpulimitl
-let yyml=num_threads+1
+# sudo cgcreate -t $USER:$USER -a $USER:$USER  -g cpuset:/cpulimitl
+trd=$1
+version=$2
+let yyml=trd+1
+defvalue=1000
+batch=${2:-$defvalue}
 # sudo cgset -r cpuset.mems=0 cpulimitl
-# sudo cgset -r cpuset.cpus=$cpuset_cpus cpulimitl
-# sudo cgset -r cpu.cfs_quota_us=$((cfs_quota_us * 1000)) cpulimitl
-# sudo cgset -r cpu.shares=0 cpulimitl
-
+# sudo cgset -r cpuset.cpus=0-$trd cpulimitl
 # sudo cgexec -g cpuset:cpulimitl ./out-perf.masstree/benchmarks/dbtest --verbose --bench tpcc --db-type mbta --scale-factor $trd --num-threads $trd --numa-memory 1G --parallel-loading --runtime 30 --bench-opts="--cpu-gap 1 --num-cpus 32" -F third-party/paxos/config/1silo_1paxos_2follower/$yyml.yml -F third-party/paxos/config/occ_paxos.yml --paxos-leader-config --multi-process -P localhost -S $batch # > ./xxxx15/leader-$trd-$batch.log 2>&1 &
 ./out-perf.masstree/benchmarks/dbtest --verbose --bench tpcc --db-type mbta --scale-factor $trd --num-threads $trd --numa-memory 1G --parallel-loading --runtime 30 --bench-opts="--cpu-gap 1 --num-cpus 32" -F third-party/paxos/config/1silo_1paxos_2follower/$yyml.yml -F third-party/paxos/config/occ_paxos.yml --paxos-leader-config --multi-process -P localhost -S $batch # > ./xxxx15/leader-$trd-$batch.log 2>&1 &
 
--- b1.sh
+++ b1.sh
@@ -1,35 +1,13 @@
 #!/bin/bash
-
-# if [ $# -lt 2 ]; then
-#   echo "Usage: $0 <num_threads> <cfs_quota_us>"
-#   exit 1
-# fi
-
-num_threads=$1
-# cfs_quota_us=$2
-
-# if [ "$cfs_quota_us" -lt 0 ] || [ "$cfs_quota_us" -gt 100 ]; then
-  # echo "cfs_quota_us must be between 0 and 100"
-  # exit 1
-# fi
-
-# total_cores=$(nproc)
-# lower_bound=$(($total_cores/3 + 1))
-# upper_bound=$(($total_cores*2/3))
-# upper_bound=$(($upper_bound < ($num_threads + $lower_bound) ? $upper_bound : ($num_threads + $lower_bound)))
-
-# cpuset_cpus="$lower_bound-$upper_bound"
-
 # sudo cgdelete -g cpuset:/cpulimitf
 mkdir -p xxxx15
-# sudo cgcreate -t $USER:$USER -a $USER:$USER -g cpuset:/cpulimitf
-let yyml=num_threads+1
+# sudo cgcreate -t $USER:$USER -a $USER:$USER  -g cpuset:/cpulimitf
+trd=$1
+let yyml=trd+1
 # sudo cgset -r cpuset.mems=0 cpulimitf
-# sudo cgset -r cpuset.cpus=$cpuset_cpus cpulimitf
-# sudo cgset -r cpu.cfs_quota_us=$((cfs_quota_us * 1000)) cpulimitf
-# sudo cgset -r cpu.shares=0 cpulimitf
-
-# sudo cgexec -g cpuset:cpulimitf ./out-perf.masstree/benchmarks/dbtest --verbose --bench tpcc --db-type mbta --scale-factor $trd --num-threads $trd --numa-memory 1G --parallel-loading --runtime 30 --bench-opts="--cpu-gap 1 --num-cpus 32" -F third-party/paxos/config/1silo_1paxos_2follower/$yyml.yml -F third-party/paxos/config/occ_paxos.yml --multi-process -P p2 # > ./xxxx15/follower-$trd.log 2>&1 &
-./out-perf.masstree/benchmarks/dbtest --verbose --bench tpcc --db-type mbta --scale-factor $trd --num-threads $trd --numa-memory 1G --parallel-loading --runtime 30 --bench-opts="--cpu-gap 1 --num-cpus 32" -F third-party/paxos/config/1silo_1paxos_2follower/$yyml.yml -F third-party/paxos/config/occ_paxos.yml --multi-process -P p2 # > ./xxxx15/follower-$trd.log 2>&1 &
+#sudo cgset -r cpuset.cpus=0-$trd cpulimitf
+# sudo cgset -r cpuset.cpus=0-$trd cpulimitf
+# sudo cgexec -g cpuset:cpulimitf ./out-perf.masstree/benchmarks/dbtest --verbose --bench tpcc --db-type mbta --scale-factor $trd --num-threads $trd --numa-memory 1G --parallel-loading --runtime 30 --bench-opts="--cpu-gap 1 --num-cpus 32" -F third-party/paxos/config/1silo_1paxos_2follower/$yyml.yml -F third-party/paxos/config/occ_paxos.yml --multi-process -P p1 # > ./xxxx15/follower-$trd.log 2>&1 &
+./out-perf.masstree/benchmarks/dbtest --verbose --bench tpcc --db-type mbta --scale-factor $trd --num-threads $trd --numa-memory 1G --parallel-loading --runtime 30 --bench-opts="--cpu-gap 1 --num-cpus 32" -F third-party/paxos/config/1silo_1paxos_2follower/$yyml.yml -F third-party/paxos/config/occ_paxos.yml --multi-process -P p1 # > ./xxxx15/follower-$trd.log 2>&1 &
 
 #tail -f ./xxxx15/follower-$trd.log
